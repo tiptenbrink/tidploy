3a63cc331eaeb164ad00f1f6af95a45afd23d06c
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-04 23:33:02 +0100

    ready for testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-#![allow(dead_code, unused_variables)]
-
 use clap::{Parser, ValueEnum, Subcommand};
 use rpassword::prompt_password;
 use keyring::{Entry, Result as KeyringResult, Error as KeyringError, Error::NoEntry};
 use serde::Deserialize;
-use std::{process::{Command as Cmd, Output, Stdio}, io::BufReader, io::BufRead, fs};
-use spinoff::{Spinner, spinners, Streams};
+use std::{process::{Command as Cmd, Stdio}, io::BufReader, io::BufRead, fs, collections::HashMap, path::Path};
+use spinoff::{Spinner, spinners};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 enum Environment {
@@ -18,7 +16,7 @@ enum Environment {
 }
 
 impl Environment {
-    fn to_string(self: Self) -> &'static str {
+    fn to_string(self) -> &'static str {
         match self {
             Self::Localdev => "localdev",
             Self::Staging => "staging",
@@ -36,7 +34,7 @@ enum Stage {
 }
 
 impl Stage {
-    fn to_string(self: Self) -> &'static str {
+    fn to_string(self) -> &'static str {
         match self {
             Self::Download => "download",
             Self::Deploy => "deploy"
@@ -44,20 +42,12 @@ impl Stage {
     }
 }
 
-/// Simple program to greet a person
+/// Deploy self-contained deploy units
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None, args_conflicts_with_subcommands(true))]
+#[command(author, version, about, long_about = None)]
 struct Args {
-    /// Environment
-    #[arg(value_enum, required = true)]
-    env: Option<Environment>,
-
-    /// Version or tag to deploy
-    #[arg(id = "version_tag", default_value = "latest")]
-    tag: Option<String>,
-
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
@@ -73,7 +63,7 @@ enum Commands {
         tag: String,
     },
 
-    /// Download tag or version with specific env
+    /// Deploy tag or version with specific env
     Deploy {
         /// Environment
         #[arg(value_enum)]
@@ -130,11 +120,11 @@ fn location(env: &str, tag: &str) -> String {
 //         .output().unwrap();
 // }
 
-fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str) -> () {
+fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str) {
     mk_tmp_dir();
 
     let archives_dir = format!("{}/archives", TMP_DIR);
-    let mk_tmp_dir = Cmd::new("mkdir")
+    let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(&archives_dir)
         .output().unwrap();
@@ -142,7 +132,7 @@ fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str)
 
     let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
 
-    let remove_existing = Cmd::new("rm")
+    let _remove_existing = Cmd::new("rm")
         .arg(&archive_loc)
         .output().unwrap();
 
@@ -158,23 +148,23 @@ fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str)
     println!("Saved deploy archive in tmp.");
 }
 
-fn download_tag(repo_url: &str, env: &str, tag: &str) -> () {
+fn download_tag(repo_url: &str, env: &str, tag: &str) {
     let loc_str = location(env, tag);
     let repo_loc = format!("{}_repo", loc_str);
     
-    let mk_tmp_dir = Cmd::new("mkdir")
+    let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(TMP_DIR)
         .output().unwrap();
 
-    let remove_existing = Cmd::new("rm")
+    let _remove_existing = Cmd::new("rm")
         .arg("-rf")
         .arg(&repo_loc)
         .output().unwrap();
 
     let mut sp = Spinner::new(spinners::Line, "Cloning repository...", None);
     
-    let repo_clone_stdout = Cmd::new("git")
+    let _repo_clone_stdout = Cmd::new("git")
         .arg("clone")
         .arg("--filter=tree:0")
         .arg(repo_url)
@@ -184,7 +174,7 @@ fn download_tag(repo_url: &str, env: &str, tag: &str) -> () {
 
     sp.success("Repository cloned!");
 
-    let checkout = Cmd::new("git")
+    let _checkout = Cmd::new("git")
         .current_dir(&repo_loc)
         .arg("checkout")
         .arg(tag)
@@ -198,41 +188,42 @@ fn download_tag(repo_url: &str, env: &str, tag: &str) -> () {
 }
 
 
-fn copy_to_archives(env: &str, tag: &str) -> () {
-    mk_tmp_dir();
+// fn copy_to_archives(env: &str, tag: &str) {
+//     mk_tmp_dir();
 
-    let archives_dir = format!("{}/archives", TMP_DIR);
-    let mk_tmp_dir = Cmd::new("mkdir")
-        .arg("-p")
-        .arg(&archives_dir)
-        .output().unwrap();
+//     let archives_dir = format!("{}/archives", TMP_DIR);
+//     let _mk_tmp_dir = Cmd::new("mkdir")
+//         .arg("-p")
+//         .arg(&archives_dir)
+//         .output().unwrap();
 
-    let env_tag = env_tag_name(env, tag);
-    let archive_name = format!("{}.tar.gz", &env_tag);
+//     let env_tag = env_tag_name(env, tag);
+//     let archive_name = format!("{}.tar.gz", &env_tag);
 
-    let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
+//     let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
 
-    let remove_existing = Cmd::new("rm")
-        .arg(&archive_loc)
-        .output().unwrap();
+//     let _remove_existing = Cmd::new("rm")
+//         .arg(&archive_loc)
+//         .output().unwrap();
     
 
-    let copy_archive = Cmd::new("cp")
-        .arg(format!("./{}", &archive_name))
-        .arg(&archive_loc)
-        .output().unwrap();
+//     let _copy_archive = Cmd::new("cp")
+//         .arg(format!("./{}", &archive_name))
+//         .arg(&archive_loc)
+//         .output().unwrap();
 
-    println!("Copied archive {} to tmp archives.", archive_name);
-}
+//     println!("Copied archive {} to tmp archives.", archive_name);
+// }
 
-fn mk_tmp_dir() -> () {
-    let mk_tmp_dir = Cmd::new("mkdir")
+fn mk_tmp_dir() {
+    let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(TMP_DIR)
         .output().unwrap();
 }
 
-fn extract(env: &str, tag: &str) -> () {
+
+fn extract(env: &str, tag: &str) {
     let archives_dir = format!("{}/archives", TMP_DIR);
     let env_tag = env_tag_name(env, tag);
     let archive_name = format!("{}.tar.gz", &env_tag);
@@ -240,12 +231,12 @@ fn extract(env: &str, tag: &str) -> () {
     let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
     let target_dir = format!("{}/{}", TMP_DIR, env_tag);
 
-    let remove_existing = Cmd::new("rm")
+    let _remove_existing = Cmd::new("rm")
         .arg("-rf")
         .arg(&target_dir)
         .output().unwrap();
 
-    let mk_tmp_dir = Cmd::new("mkdir")
+    let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(&target_dir)
         .output().unwrap();
@@ -264,7 +255,6 @@ fn extract(env: &str, tag: &str) -> () {
     tar_prog.output().unwrap();
 
     println!("Extracted archive {}.", archive_name);
-    ()
 }
 
 #[derive(Debug)]
@@ -273,23 +263,28 @@ enum Error {
     KeyringError(KeyringError)
 }
 
-fn add_password_maybe<'a, 'b>(env: Environment, cmd: &'a mut Cmd, stage: Stage, env_key: &'b str) -> Result<&'a mut Cmd, Error> {
+fn get_password_env(env: Environment, stage: Stage) -> Result<Option<String>, Error> {
     match env {
-        Environment::Localdev => Ok(cmd),
-        Environment::Staging | Environment::Production => {
-            match get_password(stage.to_string()) {
-                Ok(Some(password)) => Ok(cmd.env(env_key, password)),
-                Ok(None) => Err(Error::NoPassword),
-                Err(e) => Err(Error::KeyringError(e))
-            }
+        Environment::Localdev => Ok(None),
+        Environment::Staging | Environment::Production => match get_password(stage.to_string()) {
+            Ok(None) => Err(Error::NoPassword),
+            Ok(pw_some) => Ok(pw_some),
+            Err(e) => Err(Error::KeyringError(e))
         }
     }
 }
 
-fn show_cmd_result(output: &Output) {
-    println!("{}", output.status);
-    println!("{}", String::from_utf8(output.stderr.clone()).unwrap());
-    println!("{}", String::from_utf8(output.stdout.clone()).unwrap());
+fn add_password_maybe<'a>(cmd: &'a mut Cmd, password_option: Option<String>, env_key: &str) -> &'a mut Cmd {
+    match password_option {
+        None => cmd,
+        Some(password) => cmd.env(env_key, password)
+    }
+}
+
+#[derive(Deserialize)]
+struct SecretOutput {
+    key: String,
+    value: String,
 }
 
 #[derive(Deserialize)]
@@ -299,7 +294,7 @@ struct DployConfig {
 
 #[derive(Deserialize)]
 struct DploySecrets {
-    env_var_name: String
+    ids: Vec<String>
 }
 
 fn load_dploy_config(file_path: &str) -> DployConfig {
@@ -313,32 +308,39 @@ fn load_dploy_config(file_path: &str) -> DployConfig {
 fn main() {
     let args = Args::parse();
 
-    let repo = "DSAV-Dodeka/dodeka";
     let repo_url = "https://github.com/DSAV-Dodeka/dodeka.git";
 
     println!("{:?}", args);
 
     match args.command {
-        Some(Commands::Auth { stage }) => {
+        Commands::Auth { stage } => {
             let password = prompt_password("Enter password:\n").unwrap();
             set_password(&password, stage.to_string()).unwrap();
             println!("Set password!");
-            return ()
         },
-        Some(Commands::Download { env, tag }) => {
+        Commands::Download { env, tag } => {
             let env_str = env.to_string();
-            let tag = "ea0f030";
-            download_tag(repo_url, env_str, tag);
-            //TODO download step
+            download_tag(repo_url, env_str, &tag);
             extract(env_str, &tag);
-
-            return ()
         },
-        Some(Commands::Deploy { env, tag }) => {
+        Commands::Deploy { env, tag } => {
 
             let env_str = env.to_string();
 
-            copy_to_archives(env_str, &tag);
+            let archives_dir = format!("{}/archives", TMP_DIR);
+            let env_tag = env_tag_name(env_str, &tag);
+            let archive_name = format!("{}.tar.gz", &env_tag);
+
+            let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
+            
+            let archive_path = Path::new(&archive_loc);
+
+            if !archive_path.exists() {
+                println!("Archive doesn't exist, downloading...");
+                download_tag(repo_url, env_str, &tag);
+            }
+
+            //copy_to_archives(env_str, &tag);
             extract(env_str, &tag);
 
             println!("Running deploy.");
@@ -347,24 +349,47 @@ fn main() {
 
             let config_path = format!("{}/{}", &loc_str, "tidploy.toml");
             let dploy_config = load_dploy_config(&config_path);
-            let secret_env_key = dploy_config.secrets.env_var_name;
+
+            let maybe_password = match get_password_env(env, Stage::Deploy) {
+                Err(Error::NoPassword) => {
+                    println!("Set password using `tidploy auth`!");
+                    return
+                },
+                other => other
+            }.unwrap();
 
             let mut sp = Spinner::new(spinners::Line, "Loading secrets...", None);
-            let mut run_secrets = Cmd::new(format!("{}/{}", &loc_str, "secrets.sh"));
-            let pw_cmd = add_password_maybe(env, &mut run_secrets, Stage::Deploy, &secret_env_key);
-            if pw_cmd.as_ref().is_err_and(|e| matches!(e, Error::NoPassword)) {
-                println!("Set password using `dploy auth`!");
-                return ()
-            }
-            let run_secrets = pw_cmd.unwrap();
-            let secrets_output = run_secrets.output().unwrap();
-            sp.success("Secrets loaded into environment!");
-            show_cmd_result(&secrets_output);
+            let mut secrets = HashMap::<String, String>::new();
+            for id in dploy_config.secrets.ids {
+                let mut run_secrets = Cmd::new("bws");
+                let run_secrets = add_password_maybe(&mut run_secrets, maybe_password.clone(), "BWS_ACCESS_TOKEN")
+                    .arg("secret")
+                    .arg("get")
+                    .arg(&id);
+                let output = run_secrets.output().unwrap();
 
-            let deploy_name = format!("{}_{}", env_str, &tag).replace(".", "dot");
+                if !output.status.success() {
+                    if !output.stderr.is_empty() {
+                        println!("{}", String::from_utf8(output.stderr).unwrap());
+                    } else {
+                        println!("Error loading secrets: {:?}!", output.status)
+                    }
+                    return
+                }
+
+                let secrets_output = String::from_utf8(output.stdout).unwrap();
+
+
+                let s_output: SecretOutput = serde_json::from_str(&secrets_output).unwrap();
+                secrets.insert(s_output.key, s_output.value);
+            }
+            sp.success("Secrets loaded into environment!");
+
+            let deploy_name = format!("{}-{}", env_str, &tag).replace('.', "dot");
 
             let mut entrypoint_output = Cmd::new(format!("{}/{}", &loc_str, "entrypoint.sh"))
                 .current_dir(&loc_str)
+                .envs(&secrets)
                 .env("DEPLOY_NAME", deploy_name)
                 .stdout(Stdio::piped())
                 .spawn()
@@ -376,17 +401,16 @@ fn main() {
 
             reader
                 .lines()
-                .filter_map(|line| line.ok())
+                .map_while(Result::ok)
                 .for_each(|line| println!("{}", line));
 
             let output_stderr = entrypoint_output.wait_with_output().unwrap().stderr;
-            if output_stderr.len() > 0 {
+            if !output_stderr.is_empty() {
                 println!("{}", String::from_utf8(output_stderr).unwrap());
             }
             
 
         }
-        None => {}
     }
 
     
--- staging_v2.1/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export COMPOSE_PROJECT_NAME="dodeka${DEPLOY_NAME}"
-export NETWORK_NAME="dodeka${DEPLOY_NAME}"
-docker compose --env-file staging.env --profile all pull
-docker compose --env-file staging.env --profile all up -d
