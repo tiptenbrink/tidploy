2a8105dab02d53a92e35aba87bd9c179912e64b5
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2023-12-05 14:59:28 +0100

    0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use clap::{Parser, ValueEnum, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
+use keyring::{Entry, Error as KeyringError, Error::NoEntry, Result as KeyringResult};
 use rpassword::prompt_password;
-use keyring::{Entry, Result as KeyringResult, Error as KeyringError, Error::NoEntry};
 use serde::Deserialize;
-use std::{process::{Command as Cmd, Stdio}, io::BufReader, io::BufRead, fs, collections::HashMap, path::Path};
-use spinoff::{Spinner, spinners};
+use spinoff::{spinners, Spinner};
+use std::{
+    collections::HashMap,
+    fs,
+    io::BufRead,
+    io::BufReader,
+    path::Path,
+    process::{Command as Cmd, Stdio},
+};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 enum Environment {
@@ -12,7 +19,7 @@ enum Environment {
     /// Staging environment
     Staging,
     /// Production environment
-    Production
+    Production,
 }
 
 impl Environment {
@@ -20,7 +27,7 @@ impl Environment {
         match self {
             Self::Localdev => "localdev",
             Self::Staging => "staging",
-            Self::Production => "production"
+            Self::Production => "production",
         }
     }
 }
@@ -37,7 +44,7 @@ impl Stage {
     fn to_string(self) -> &'static str {
         match self {
             Self::Download => "download",
-            Self::Deploy => "deploy"
+            Self::Deploy => "deploy",
         }
     }
 }
@@ -59,8 +66,7 @@ enum Commands {
         env: Environment,
 
         /// Version or tag to download
-        #[arg(id = "version_tag", default_value = "latest")]
-        tag: String,
+        git_ref: Option<String>,
     },
 
     /// Deploy tag or version with specific env
@@ -69,15 +75,22 @@ enum Commands {
         #[arg(value_enum)]
         env: Environment,
 
-        /// Version or tag to deploy
-        #[arg(id = "version_tag", default_value = "latest")]
-        tag: String,
+        /// Version or tag to deploy. Omit to deploy latest for env
+        git_ref: Option<String>,
+
+        /// Whether to get the latest version of the ref (default: true)
+        #[arg(id = "latest", short, long, default_value_t = true)]
+        latest_opt: bool,
+
+        /// Whether to recreate the database (default: false)
+        #[arg(short, long, default_value_t = false)]
+        recreate: bool,
     },
-    
+
     /// Save authentication details for specific stage until reboot
     Auth {
         #[arg(value_enum)]
-        stage: Stage
+        stage: Stage,
     },
 }
 
@@ -104,7 +117,7 @@ fn env_tag_name(env: &str, tag: &str) -> String {
 
 fn location(env: &str, tag: &str) -> String {
     let env_tag_name = env_tag_name(env, tag);
-    
+
     format!("{}/{}", TMP_DIR, env_tag_name)
 }
 
@@ -127,14 +140,13 @@ fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str)
     let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(&archives_dir)
-        .output().unwrap();
+        .output()
+        .unwrap();
     let archive_name = format!("{}.tar.gz", env_tag_name(env, tag));
 
     let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
 
-    let _remove_existing = Cmd::new("rm")
-        .arg(&archive_loc)
-        .output().unwrap();
+    let _remove_existing = Cmd::new("rm").arg(&archive_loc).output().unwrap();
 
     let mut output_archive_prog = Cmd::new("tar");
     let output_archive = output_archive_prog
@@ -148,45 +160,76 @@ fn make_archive(source_dir_parent: &str, source_dir: &str, env: &str, tag: &str)
     println!("Saved deploy archive in tmp.");
 }
 
-fn download_tag(repo_url: &str, env: &str, tag: &str) {
+fn checkout_tag(repo_loc: &str, git_ref: &str) {
+    let _checkout = Cmd::new("git")
+        .current_dir(repo_loc)
+        .arg("checkout")
+        .arg("-f")
+        .arg(git_ref)
+        .output()
+        .unwrap();
+}
+
+fn create_archive(repo_url: &str, env: &str, tag: &str, git_ref_opt: Option<String>, latest: bool) {
     let loc_str = location(env, tag);
     let repo_loc = format!("{}_repo", loc_str);
-    
-    let _mk_tmp_dir = Cmd::new("mkdir")
-        .arg("-p")
-        .arg(TMP_DIR)
-        .output().unwrap();
 
-    let _remove_existing = Cmd::new("rm")
-        .arg("-rf")
-        .arg(&repo_loc)
-        .output().unwrap();
+    mk_tmp_dir();
 
-    let mut sp = Spinner::new(spinners::Line, "Cloning repository...", None);
-    
-    let _repo_clone_stdout = Cmd::new("git")
-        .arg("clone")
-        .arg("--filter=tree:0")
-        .arg(repo_url)
-        .arg(&repo_loc)
-        .stdout(Stdio::piped())
-        .output().unwrap();
+    let exists = Path::new(&repo_loc).exists();
 
-    sp.success("Repository cloned!");
+    if !exists || git_ref_opt.is_none() {
+        let _remove_existing = Cmd::new("rm").arg("-rf").arg(&repo_loc).output().unwrap();
 
-    let _checkout = Cmd::new("git")
-        .current_dir(&repo_loc)
-        .arg("checkout")
-        .arg(tag)
-        .output().unwrap();
+        let mut sp = Spinner::new(spinners::Line, "Cloning repository...", None);
 
-    println!("Checked out ref {}.", tag);
+        let _repo_clone_stdout = Cmd::new("git")
+            .arg("clone")
+            .arg("--filter=tree:0")
+            .arg(repo_url)
+            .arg(&repo_loc)
+            .stdout(Stdio::piped())
+            .output()
+            .unwrap();
+
+        sp.success("Repository cloned!");
+
+        if let Some(git_ref) = git_ref_opt {
+            let mut sp = Spinner::new(spinners::Line, "Checking out ref...", None);
+
+            checkout_tag(&repo_loc, &git_ref);
+
+            sp.success("Checked out ref!");
+        }
+    } else if let Some(git_ref) = git_ref_opt {
+        if exists && latest {
+            let mut sp = Spinner::new(spinners::Line, "Checking out ref and updating...", None);
+
+            checkout_tag(&repo_loc, &git_ref);
+
+            // In case we were on a branch we now update to latest
+            let _pull = Cmd::new("git")
+                .current_dir(&repo_loc)
+                .arg("pull")
+                .output()
+                .unwrap();
+
+            checkout_tag(&repo_loc, &git_ref);
+
+            sp.success("Checked out ref!");
+        } else if exists {
+            let mut sp = Spinner::new(spinners::Line, "Checking out ref...", None);
+
+            checkout_tag(&repo_loc, &git_ref);
+
+            sp.success("Checked out ref!");
+        }
+    }
 
     let use_dir = format!("{}/use", repo_loc);
 
     make_archive(&use_dir, env, env, tag);
 }
-
 
 // fn copy_to_archives(env: &str, tag: &str) {
 //     mk_tmp_dir();
@@ -205,7 +248,6 @@ fn download_tag(repo_url: &str, env: &str, tag: &str) {
 //     let _remove_existing = Cmd::new("rm")
 //         .arg(&archive_loc)
 //         .output().unwrap();
-    
 
 //     let _copy_archive = Cmd::new("cp")
 //         .arg(format!("./{}", &archive_name))
@@ -216,12 +258,8 @@ fn download_tag(repo_url: &str, env: &str, tag: &str) {
 // }
 
 fn mk_tmp_dir() {
-    let _mk_tmp_dir = Cmd::new("mkdir")
-        .arg("-p")
-        .arg(TMP_DIR)
-        .output().unwrap();
+    let _mk_tmp_dir = Cmd::new("mkdir").arg("-p").arg(TMP_DIR).output().unwrap();
 }
-
 
 fn extract(env: &str, tag: &str) {
     let archives_dir = format!("{}/archives", TMP_DIR);
@@ -231,20 +269,19 @@ fn extract(env: &str, tag: &str) {
     let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
     let target_dir = format!("{}/{}", TMP_DIR, env_tag);
 
-    let _remove_existing = Cmd::new("rm")
-        .arg("-rf")
-        .arg(&target_dir)
-        .output().unwrap();
+    let _remove_existing = Cmd::new("rm").arg("-rf").arg(&target_dir).output().unwrap();
 
     let _mk_tmp_dir = Cmd::new("mkdir")
         .arg("-p")
         .arg(&target_dir)
-        .output().unwrap();
+        .output()
+        .unwrap();
 
     let mut tar_prog = Cmd::new("tar");
     //tar -xzf /tmp/ti_dploy/archives/staging_b648930.tar.gz -C ./staging_b648930 --strip-components 1
     // strip components might not work on every platform
-    let tar_prog = tar_prog.arg("-xzf")
+    let tar_prog = tar_prog
+        .arg("-xzf")
         .arg(archive_loc)
         .current_dir(TMP_DIR)
         .arg("-C")
@@ -260,7 +297,7 @@ fn extract(env: &str, tag: &str) {
 #[derive(Debug)]
 enum Error {
     NoPassword,
-    KeyringError(KeyringError)
+    KeyringError(KeyringError),
 }
 
 fn get_password_env(env: Environment, stage: Stage) -> Result<Option<String>, Error> {
@@ -269,15 +306,19 @@ fn get_password_env(env: Environment, stage: Stage) -> Result<Option<String>, Er
         Environment::Staging | Environment::Production => match get_password(stage.to_string()) {
             Ok(None) => Err(Error::NoPassword),
             Ok(pw_some) => Ok(pw_some),
-            Err(e) => Err(Error::KeyringError(e))
-        }
+            Err(e) => Err(Error::KeyringError(e)),
+        },
     }
 }
 
-fn add_password_maybe<'a>(cmd: &'a mut Cmd, password_option: Option<String>, env_key: &str) -> &'a mut Cmd {
+fn add_password_maybe<'a>(
+    cmd: &'a mut Cmd,
+    password_option: Option<String>,
+    env_key: &str,
+) -> &'a mut Cmd {
     match password_option {
         None => cmd,
-        Some(password) => cmd.env(env_key, password)
+        Some(password) => cmd.env(env_key, password),
     }
 }
 
@@ -289,12 +330,18 @@ struct SecretOutput {
 
 #[derive(Deserialize)]
 struct DployConfig {
-   secrets: DploySecrets,
+    secrets: DploySecrets,
+    info: DployInfo,
+}
+
+#[derive(Deserialize)]
+struct DployInfo {
+    latest: String,
 }
 
 #[derive(Deserialize)]
 struct DploySecrets {
-    ids: Vec<String>
+    ids: Vec<String>,
 }
 
 fn load_dploy_config(file_path: &str) -> DployConfig {
@@ -310,20 +357,38 @@ fn main() {
 
     let repo_url = "https://github.com/DSAV-Dodeka/dodeka.git";
 
-    println!("{:?}", args);
+    //println!("{:?}", args);
 
     match args.command {
         Commands::Auth { stage } => {
             let password = prompt_password("Enter password:\n").unwrap();
             set_password(&password, stage.to_string()).unwrap();
             println!("Set password!");
-        },
-        Commands::Download { env, tag } => {
+        }
+        Commands::Download { env, git_ref } => {
             let env_str = env.to_string();
-            download_tag(repo_url, env_str, &tag);
+            let tag = match &git_ref {
+                Some(git_ref) => git_ref.clone(),
+                None => "latest".to_owned(),
+            };
+            create_archive(repo_url, env_str, &tag, git_ref, true);
             extract(env_str, &tag);
-        },
-        Commands::Deploy { env, tag } => {
+        }
+        Commands::Deploy {
+            env,
+            git_ref,
+            latest_opt,
+            recreate,
+        } => {
+            let mut latest = latest_opt;
+            if git_ref.is_none() && !latest {
+                println!("No git ref is specified, setting latest to true!");
+                latest = false;
+            }
+            let tag = match &git_ref {
+                Some(git_ref) => git_ref.clone(),
+                None => "latest".to_owned(),
+            };
 
             let env_str = env.to_string();
 
@@ -332,40 +397,66 @@ fn main() {
             let archive_name = format!("{}.tar.gz", &env_tag);
 
             let archive_loc = format!("{}/{}", &archives_dir, &archive_name);
-            
+
             let archive_path = Path::new(&archive_loc);
 
-            if !archive_path.exists() {
-                println!("Archive doesn't exist, downloading...");
-                download_tag(repo_url, env_str, &tag);
+            // Always download if tag is latest
+            let new_archive = !archive_path.exists() || latest;
+
+            if new_archive {
+                println!("Creating new archive...");
+                create_archive(repo_url, env_str, &tag, git_ref, latest);
             }
 
-            //copy_to_archives(env_str, &tag);
             extract(env_str, &tag);
-
-            println!("Running deploy.");
 
             let loc_str = location(env_str, &tag);
 
             let config_path = format!("{}/{}", &loc_str, "tidploy.toml");
-            let dploy_config = load_dploy_config(&config_path);
+            let mut dploy_config = load_dploy_config(&config_path);
+
+            // in this case we are on the latest commit, but we need to go back to the correct commit of the latest release
+            if latest && new_archive {
+                // Redownload with correct tag
+                create_archive(
+                    repo_url,
+                    env_str,
+                    &tag,
+                    Some(dploy_config.info.latest.clone()),
+                    true,
+                );
+                // Reload config
+                extract(env_str, &tag);
+                dploy_config = load_dploy_config(&config_path);
+            }
+
+            println!("Running deploy.");
 
             let maybe_password = match get_password_env(env, Stage::Deploy) {
                 Err(Error::NoPassword) => {
                     println!("Set password using `tidploy auth`!");
-                    return
-                },
-                other => other
-            }.unwrap();
+                    return;
+                }
+                other => other,
+            }
+            .unwrap();
+
+            // if true {
+            //     return;
+            // }
 
             let mut sp = Spinner::new(spinners::Line, "Loading secrets...", None);
             let mut secrets = HashMap::<String, String>::new();
             for id in dploy_config.secrets.ids {
                 let mut run_secrets = Cmd::new("bws");
-                let run_secrets = add_password_maybe(&mut run_secrets, maybe_password.clone(), "BWS_ACCESS_TOKEN")
-                    .arg("secret")
-                    .arg("get")
-                    .arg(&id);
+                let run_secrets = add_password_maybe(
+                    &mut run_secrets,
+                    maybe_password.clone(),
+                    "BWS_ACCESS_TOKEN",
+                )
+                .arg("secret")
+                .arg("get")
+                .arg(&id);
                 let output = run_secrets.output().unwrap();
 
                 if !output.status.success() {
@@ -374,23 +465,35 @@ fn main() {
                     } else {
                         println!("Error loading secrets: {:?}!", output.status)
                     }
-                    return
+                    return;
                 }
 
                 let secrets_output = String::from_utf8(output.stdout).unwrap();
-
 
                 let s_output: SecretOutput = serde_json::from_str(&secrets_output).unwrap();
                 secrets.insert(s_output.key, s_output.value);
             }
             sp.success("Secrets loaded into environment!");
 
-            let deploy_name = format!("{}-{}", env_str, &tag).replace('.', "dot");
+            let deploy_name = format!("{}-{}", env_str, &tag).replace('.', "_");
+
+            let recreate_value = if recreate { "yes" } else { "no" };
+
+            // TODO this is too specific logic
+            let deploy_tag_suffix = if tag == "latest" {
+                "".to_owned()
+            } else {
+                format!("-{}", &tag)
+            };
+
+            println!("Running entrypoint with deploy name {}...", &deploy_name);
 
             let mut entrypoint_output = Cmd::new(format!("{}/{}", &loc_str, "entrypoint.sh"))
                 .current_dir(&loc_str)
                 .envs(&secrets)
+                .env("RECREATE", recreate_value)
                 .env("DEPLOY_NAME", deploy_name)
+                .env("DEPLOY_TAG_SUFFIX", &deploy_tag_suffix)
                 .stdout(Stdio::piped())
                 .spawn()
                 .unwrap();
@@ -408,10 +511,6 @@ fn main() {
             if !output_stderr.is_empty() {
                 println!("{}", String::from_utf8(output_stderr).unwrap());
             }
-            
-
         }
     }
-
-    
 }
